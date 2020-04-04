531177fee6054efac169a7ee10608dd83db752eb
luc1dLife <luc1d_life@protonmail.com>
2020-04-03 22:26:45 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+<img align="right" src="Etherscan.io/waviii.gif">
+
+![iiiWallet_logo](src/iiiWallet_logo_small.png) 
 # iiiWallet
+
 Official waviii Token Wallet
+
+─── [Access iiiWallet](https://etherscan.io/token/0xBA00868912Af1a409F11E9c2B5d3a9376Cb3C2E2)<br />
+─── [waviii Token](https://github.com/luc1dLife/waviii)
+
+    [node v10.15.2]
+    [npm v5.8.0]
+    [Truffle v5.0.5]
+
+    Total Supply: 1,000 waviii
+
+    Contract Address: 0xBA00868912Af1a409F11E9c2B5d3a9376Cb3C2E2
---
+++ src/components/App.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import waviiiLogo from '../../src/iiiWallet.png';
+import './App.css';
+import Web3 from 'web3';
+import waviii from '../abis/waviii.json'
+
+class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const daiTokenAddress = "0xBA00868912Af1a409F11E9c2B5d3a9376Cb3C2E2" // waviii Token Contract Address
+    const waviiiToken = new web3.eth.Contract(waviii.abi, daiTokenAddress)
+    this.setState({ waviiiToken: waviiiToken })
+    const balance = await waviiiToken.methods.balanceOf(this.state.account).call()
+    this.setState({ balance: web3.utils.fromWei(balance.toString(), 'Ether') })
+    const transactions = await waviiiToken.getPastEvents('Transfer', { fromBlock: 0, toBlock: 'latest', filter: { from: this.state.account } })
+    this.setState({ transactions: transactions })
+    console.log(transactions)
+  }
+
+  transfer(recipient, amount) {
+    this.state.waviiiToken.methods.transfer(recipient, amount).send({ from: this.state.account })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      waviiiToken: null,
+      balance: 0,
+      transactions: []
+    }
+
+    this.transfer = this.transfer.bind(this)
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="https://github.com/luc1dLife/iiiWallet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            iiiWallet
+          </a>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto" style={{ width: "500px" }}>
+                <a
+                  href="https://github.com/luc1dLife/waviii"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={waviiiLogo} width="150" />
+                </a>
+                <h1>{this.state.balance} waviii</h1>
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  const recipient = this.recipient.value
+                  const amount = window.web3.utils.toWei(this.amount.value, 'Ether')
+                  this.transfer(recipient, amount)
+                }}>
+                  <div className="form-group mr-sm-2">
+                    <input
+                      id="recipient"
+                      type="text"
+                      ref={(input) => { this.recipient = input }}
+                      className="form-control"
+                      placeholder="Recipient Address"
+                      required />
+                  </div>
+                  <div className="form-group mr-sm-2">
+                    <input
+                      id="amount"
+                      type="text"
+                      ref={(input) => { this.amount = input }}
+                      className="form-control"
+                      placeholder="Amount"
+                      required />
+                  </div>
+                  <button type="submit" className="btn btn-primary btn-block">Send</button>
+                </form>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Recipient</th>
+                      <th scope="col">value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    { this.state.transactions.map((tx, key) => {
+                      return (
+                        <tr key={key} >
+                          <td>{tx.returnValues.to}</td>
+                          <td>{window.web3.utils.fromWei(tx.returnValues.value.toString(), 'Ether')}</td>
+                        </tr>
+                      )
+                    }) }
+                  </tbody>
+                </table>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
