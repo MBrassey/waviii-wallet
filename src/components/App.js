22d195760b6c3d2cadf86e1d8ae241c67be7e3da
luc1dLife <luc1d_life@protonmail.com>
2020-04-04 17:19:12 -0600

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -57,6 +57,7 @@ class App extends Component {
   render() {
     return (
       <div>
+
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
@@ -66,18 +67,22 @@ class App extends Component {
           >
             iiiWallet
           </a>
+
+
+          <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-white"><span id="account">{this.props.account}</span></small>
+          </li>
+        </ul>
+
         </nav>
+
+
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto" style={{ width: "500px" }}>
-                <a
-                  href="https://github.com/luc1dLife/waviii"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
                   <img src={waviiiLogo} width="150" />
-                </a>
                 <h1>{this.state.balance} waviii</h1>
                 <form onSubmit={(event) => {
                   event.preventDefault()
