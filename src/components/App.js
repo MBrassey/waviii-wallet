a655ee732340f741d10492b9f2c9d2a838ebf049
luc1dLife <luc1d_life@protonmail.com>
2020-04-04 13:04:03 -0600

    updated style

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import waviiiLogo from '../../src/iiiWallet.png';
 import Web3 from 'web3';
 import waviii from '../abis/waviii.json'
 
-document.body.style = 'background: #DFE5E8;';
+document.body.style = 'background: #CED4D8;';
 
 class App extends Component {
   async componentWillMount() {
@@ -57,7 +57,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-iiiNav fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="https://github.com/luc1dLife/iiiWallet"
